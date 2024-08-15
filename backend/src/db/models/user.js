384989d82f484a89f3f2c33c51ca69aad0c20f74
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2024-08-14 22:46:05 -0300

    :recycle: limpeza de código + migration

--- backend/src/db/models/user.js
+++ backend/src/db/models/user.js
@@ -1,28 +1,48 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
   class User extends Model {
     static associate(models) {
-      
+      // Define o relacionamento de muitos Users para um UserType
+      User.belongsTo(models.UserType, {
+        foreignKey: 'userTypeId',
+        as: 'UserTypes'
+      });
     }
   }
+
   User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    userTypeId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'UserTypes',
+        key: 'id'
+      },
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'User',
+    tableName: 'Users'
   });
-  User.associate = function(models) {
-    // Define o relacionamento de muitos Users para um UserType
-    User.belongsTo(models.UserType, {
-      foreignKey: 'userTypeId',
-      as: 'UserTypes'
-    });
-  };
 
   return User;
 };
