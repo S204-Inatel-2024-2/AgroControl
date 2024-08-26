c1593b4f7fe26cfd96f1d8cce4feb1b068cf5088
guilhermecmr <guilhermecottamr@gmail.com>
2024-08-26 16:05:11 -0300

    Adiciona os endpoints /servicos

--- backend/src/db/models/servicos.js
+++ backend/src/db/models/servicos.js
@@ -7,61 +7,60 @@ module.exports = (sequelize, DataTypes) => {
 
     static associate(models) {
       //relacionamento com a tabela de Tipos de serviço
-      Servicos.belongsTo(models.Service,{
-        foreignKey:'tipoServico'
+      Servicos.belongsTo(models.Service, {
+        foreignKey: 'tipoServico'
       })
     }
 
     static associate(models) {
       //relacionamento com a tabela de Funcionários
-      Servicos.belongsTo(models.Funcionarios,{
-        foreignKey:'tipoServico'
+      Servicos.belongsTo(models.Funcionarios, {
+        foreignKey: 'tipoServico'
       })
     }
 
 
   }
   Servicos.init({
-    IdServico:{
-      type:DataTypes.INTEGER,
-      primaryKey:true,
-      autoIncrement:true
-    } ,
+    IdServico: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     status: {
-      type:DataTypes.STRING,
-      allowNull:false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     dataAtividade: {
-     type: DataTypes.DATE,
-     allowNull:false
+      type: DataTypes.DATE,
+      allowNull: false
     },
     tipoServico: {
-       type:DataTypes.INTEGER,
-       allowNull:false,
-       references: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
         model: 'TiposServico',
         key: 'id'
       }
 
     },
-    responsavel:{
-      type:DataTypes.INTEGER,
-      allowNull:false,
+    responsavel: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
       references: {
         model: 'Funcionarios',
         key: 'id'
       }
 
     },
-    valorGasto:{
-      type:DataTypes.FLOAT,
-      allowNull:false
-    }, 
+    valorGasto: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
   }, {
     sequelize,
     modelName: 'Servicos',
     tableName: 'Servicos',
-    timestamps:false
   });
   return Servicos;
 };
