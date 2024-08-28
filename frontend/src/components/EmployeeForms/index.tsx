c2428692bd48c8a32058ee22b1fc5015389912b9
Juliely <juhlima26@icloud.com>
2024-08-28 12:45:27 -0300

    feat: :art: update style

--- frontend/src/components/EmployeeForms/index.tsx
+++ frontend/src/components/EmployeeForms/index.tsx
@@ -19,8 +19,8 @@ export function EmployeeForms(): JSX.Element {
   const { register, handleSubmit, formState } = useForm<FormValues>();
   const navigate = useNavigate();
 
-  const onSubmit =  (data: FormValues) =>{
-    const funcionario ={
+  const onSubmit = (data: FormValues) => {
+    const funcionario = {
       nome: data.name,
       email: data.email,
       cpf: data.cpf,
@@ -50,99 +50,109 @@ export function EmployeeForms(): JSX.Element {
             <Styled.Button type='submit' disabled={formState.isSubmitting}>Salvar informações</Styled.Button>
             <Styled.Button onClick={() => navigate('/home')}>Voltar</Styled.Button>
           </Styled.ButtonDiv>
-        
+
         </Styled.TitleDiv>
-        
+
         <Styled.InputDiv>
           <Styled.Label>
-              Nome:
-              <Styled.Input
-                type="text"
-                placeholder='Nome'
-                disabled={formState.isSubmitting}
-                {...register('name', { required: true })}
-              />
-              {formState.errors.name && <Styled.Error>Nome é obrigatório</Styled.Error>}
-            </Styled.Label>
-            
+            Nome:
+            <Styled.Input
+              type="text"
+              placeholder='Nome'
+              disabled={formState.isSubmitting}
+              {...register('name', { required: true })}
+            />
+            {formState.errors.name && <Styled.Error>Nome é obrigatório</Styled.Error>}
+          </Styled.Label>
+          <Styled.RowDiv>
+            <Styled.InputWrapper >
               <Styled.Label>
-              CPF:
-              <Styled.Input
-                type="text"
-                placeholder='CPF'
-                {...register('cpf', { required: true })}
-                disabled={formState.isSubmitting}
-              />
-              {formState.errors.cpf && <Styled.Error>CPF é obrigatório</Styled.Error>}
-            </Styled.Label>
-            
-            <Styled.Label>
-              Data de nascimento:
-              <Styled.Input
-                type="date"
-                placeholder=''
-                {...register('birthDate', { required: true })}
-                disabled={formState.isSubmitting}
-              />
-              {formState.errors.birthDate && <Styled.Error style={{right: 30}}>Data de nascimento é obrigatória</Styled.Error>}
+                CPF:
+                <Styled.Input 
+                style={{marginLeft: 89}}
+                  type="text"
+                  placeholder='CPF'
+                  {...register('cpf', { required: true })}
+                  disabled={formState.isSubmitting}
+                />
+                {formState.errors.cpf && <Styled.Error>CPF é obrigatório</Styled.Error>}
+              </Styled.Label>
+            </Styled.InputWrapper>
+            <Styled.InputWrapper >
+              <Styled.Label>
+                Data de nascimento:
+                <Styled.Input 
+                  type="date"
+                  placeholder=''
+                  {...register('birthDate', { required: true })}
+                  disabled={formState.isSubmitting}
+                />
+                {formState.errors.birthDate && <Styled.Error style={{bottom:0}}>Data de nascimento é obrigatória</Styled.Error>}
 
-            </Styled.Label>
-            <Styled.Label>
-              Endereço:
-              <Styled.Input
-                type="text"
-                placeholder='Endereço'
-                {...register('address', { required: true })}
-                disabled={formState.isSubmitting}
-              />
-              {formState.errors.address && <Styled.Error>Endereço é obrigatório</Styled.Error>}
-            </Styled.Label>
+              </Styled.Label>
+            </Styled.InputWrapper>
+          </Styled.RowDiv>
 
-            <Styled.Label>
-              Email:
-              <Styled.Input
-                type="email"
-                placeholder='Email'
-                {...register('email', { required: true })}
-                disabled={formState.isSubmitting}
-              />
-              {formState.errors.email && <Styled.Error>Email é obrigatório</Styled.Error>}
 
-            </Styled.Label>
-            <Styled.Label>
-              Função:
-              <Styled.Input
-                type="text"
-                placeholder='Função'
-                {...register('role', { required: true })}
-                disabled={formState.isSubmitting}
-              />
-              {formState.errors.role && <Styled.Error>Função é obrigatória</Styled.Error>}
-            </Styled.Label>
 
-             <Styled.Label>
-              Valor do pagamento:
-              <Styled.Input
-                type="text"
-                placeholder='valor'
-                disabled={formState.isSubmitting}
-                {...register('payment', { required: true })}
-              />
-              {formState.errors.payment && <Styled.Error>Pagamento é obrigatório</Styled.Error>}
-            </Styled.Label>
-            <Styled.Label>
-              Observações:
-              <Styled.ObservacaoInput
-                placeholder='observações...'
-                //disabled={formState.isSubmitting}
-              />
-            </Styled.Label>
-        </Styled.InputDiv>
-        
+          <Styled.Label>
+            Endereço:
+            <Styled.Input
+              type="text"
+              placeholder='Endereço'
+              {...register('address', { required: true })}
+              disabled={formState.isSubmitting}
+            />
+            {formState.errors.address && <Styled.Error>Endereço é obrigatório</Styled.Error>}
+          </Styled.Label>
+
+          <Styled.Label>
+            Email:
+            <Styled.Input
+              type="email"
+              placeholder='Email'
+              {...register('email', { required: true })}
+              disabled={formState.isSubmitting}
+            />
+            {formState.errors.email && <Styled.Error>Email é obrigatório</Styled.Error>}
+
+          </Styled.Label>
+          <Styled.Label>
+            Função:
+            <Styled.Input
+              type="text"
+              placeholder='Função'
+              {...register('role', { required: true })}
+              disabled={formState.isSubmitting}
+            />
+            {formState.errors.role && <Styled.Error>Função é obrigatória</Styled.Error>}
+          </Styled.Label>
+          
+          
+          <Styled.Label style={{gap:20}}>
+            Valor do pagamento:
+            <Styled.Input 
+              type="text"
+              placeholder='valor'
+              disabled={formState.isSubmitting}
+              {...register('payment', { required: true })}
+            />
+            {formState.errors.payment && <Styled.Error>Pagamento é obrigatório</Styled.Error>}
+          </Styled.Label> 
+         
+          <Styled.Label >
+            Observações:
+            <Styled.ObservacaoInput
+              placeholder='observações...'
+            //disabled={formState.isSubmitting}
+            />
+          </Styled.Label>
        
-        </Styled.Form>
+          </Styled.InputDiv>
+
+      </Styled.Form>
     </Styled.Container>
-    
+
 
   );
 };
