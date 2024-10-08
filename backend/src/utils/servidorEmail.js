10a6fd31271971f76cea0fd34e70a84ed87f7036
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2024-10-08 20:53:13 -0300

    :lock: regras Criar Funcionario

--- backend/src/utils/servidorEmail.js
+++ backend/src/utils/servidorEmail.js
@@ -4,19 +4,19 @@ const emailFuncionarioCadastrado = require('../utils/templatesEmail/FuncionarioC
 // Cria o transporte utilizando MailDev como servidor SMTP
 let transporter = nodemailer.createTransport({
     host: "127.0.0.1", // Força o uso de IPv4
-    port: 1025,        // Porta padrão do MailDev
+    port: 1025,        // Porta padrão do MailDev {http://127.0.0.1:1080/#/}
     secure: false,     // Sem SSL/TLS
 });
 
 async function emailnovoFuncionario(funcionarioNovo) {
     try {
         let info = await transporter.sendMail({
-            from: `${funcionarioNovo.nome},'<${funcionarioNovo.email}>'`,
+            from: `Equipe de gerencia agroControl'<AgroControl@int.com>'`,
             to: `${funcionarioNovo.email}'`,
             subject: "Novo funcionário cadastrado",
             html: emailFuncionarioCadastrado(funcionarioNovo),
         });
-        
+
         console.log("Email enviado: %s", info.messageId);
     } catch (error) {
         console.error("Erro ao enviar email: ", error);
