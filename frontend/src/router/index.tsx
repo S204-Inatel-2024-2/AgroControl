71224e6e49c1e4763d2e1084ef41c8b4b6b85a6c
Iza Lopes <102091381+Izalp@users.noreply.github.com>
2024-10-09 19:32:11 -0300

    Puxando alterações e correções realizadas

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -6,10 +6,9 @@ import { EmployeeRegistration } from "../pages/EmployeeRegistration";
 import { RegisterFinances } from "../pages/RegisterFinances";
 import { EmployeeDetails } from "../pages/EmployeeDetails";
 import { ServiceDetails } from "../pages/ServiceDetails";
-import {RegisterReceitas} from "../pages/RegisterReceitas"
+import { RegisterReceitas } from "../pages/RegisterReceitas";
 import { Employees } from "../pages/Employees";
 import { Services } from "../pages/Services";
-import { RegisterReceitas } from "../pages/RegisterReceitas";
 
 const AppRoutes = () => {
   return (
@@ -19,7 +18,10 @@ const AppRoutes = () => {
           <Route path="" element={<LoginPage />} />
           <Route path="home" element={<Home />} />
           <Route path="employees" element={<Employees />} />
-          <Route path="employeeregistration" element={<EmployeeRegistration />} />
+          <Route
+            path="employeeregistration"
+            element={<EmployeeRegistration />}
+          />
           <Route path="registerfinances" element={<RegisterFinances />} />
           <Route path="registerReceitas" element={<RegisterReceitas />} />
           <Route path="employeedetails/:id" element={<EmployeeDetails />} />
