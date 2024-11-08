8704552a0216e3f74188043b2ee9be0549805fd7
Iza Lopes <102091381+Izalp@users.noreply.github.com>
2024-11-08 11:57:24 -0300

    :recycle: refactor: modifica estrutura e conteúdo do e-mail de novos serviços transferidos

--- backend/src/utils/templatesEmail/TransferenciaServico.js
+++ backend/src/utils/templatesEmail/TransferenciaServico.js
@@ -1,23 +1,19 @@
-module.exports = (dadosFuncionario) => {
-    const formattedServicos = dadosFuncionario.novosServicos.map(servico => `
-        <ul style="list-style-type: circle; margin-left: 20px; padding: 0;">
-            <li><strong>ID do Serviço:</strong> ${servico.IdServico}</li>
-            <li><strong>Status:</strong> ${servico.status}</li>
-            <li><strong>Data da Atividade:</strong> ${new Date(servico.dataAtividade).toLocaleDateString()}</li>
-            <li><strong>Tipo de Serviço:</strong> ${servico.tipoServico || 'N/A'}</li>
-            <li><strong>Valor Gasto:</strong> R$ ${servico.valorGasto ? servico.valorGasto.toFixed(2) : '0.00'}</li>
-            <li><strong>Data de Criação:</strong> ${new Date(servico.createdAt).toLocaleDateString()}</li>
-        </ul>
-    `).join('');
-
+module.exports = (dadosServico) => {
     return `
         <div style="font-family: Arial, sans-serif; color: #333; padding: 20px; max-width: 600px; margin: auto; border: 1px solid #ddd; border-radius: 10px;">
             <h2 style="color: #0056b3;">Notificação de Novos Serviços Atribuídos</h2>
-            <p>Olá <strong>${dadosFuncionario.nomeFuncionario}</strong>,</p>
-            <p>Você recebeu novos serviços atribuídos a você. Abaixo estão os detalhes dos serviços que foram designados:</p>
+            <p>Olá <strong>${dadosServico.novoResponsavel}</strong>,</p>
+            <p>Você recebeu um novo serviço atribuído a você. Abaixo estão os detalhes do serviço que foi designado:</p>
 
-            <h3 style="color: #0056b3;">Detalhes dos Novos Serviços:</h3>
-            ${formattedServicos}
+            <h3 style="color: #0056b3;">Detalhes do Novo Serviço:</h3>
+            <ul style="list-style-type: circle; margin-left: 20px; line-height: 1.8;">
+                <li><strong>Serviço:</strong> ${dadosServico.servico}</li>
+                <li><strong>Status:</strong> ${dadosServico.status}</li>
+                <li><strong>Data da Atividade:</strong> ${new Date(dadosServico.dataAtividade).toLocaleDateString()}</li>
+                <li><strong>Tipo de Serviço:</strong> ${dadosServico.tipoServico || 'N/A'}</li>
+                <li><strong>Valor Gasto:</strong> R$ ${dadosServico.valorGasto.toFixed(2)}</li>
+                <li><strong>Data da Transferência:</strong> ${new Date().toLocaleDateString()}</li>
+            </ul>
 
             <p>Por favor, revise os detalhes e entre em contato com seu supervisor se tiver dúvidas ou precisar de suporte adicional.</p>
 
