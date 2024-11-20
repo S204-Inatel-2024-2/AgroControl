90d3160133763b28ed26c8f8dfca99c71d200df7
Iza Lopes <102091381+Izalp@users.noreply.github.com>
2024-11-19 21:04:59 -0300

    :bug: fix: corrigir testes de funcionario e melhorar a suite para maior cobertura de testes

--- backend/src/tests/funcionario.test.js
+++ backend/src/tests/funcionario.test.js
@@ -1,5 +1,8 @@
 const { Funcionarios, Servicos } = require("../db/models");
-const { emailNovoFuncionario } = require("../utils/servidorEmail");
+const {
+  emailNovoFuncionario,
+  emailFuncionarioRemovido,
+} = require("../utils/servidorEmail");
 const FuncionarioService = require("../services/funcionario");
 
 jest.mock("../db/models", () => ({
@@ -128,6 +131,34 @@ describe("FuncionarioService", () => {
       });
       expect(emailNovoFuncionario).toHaveBeenCalledWith(req.body);
     });
+
+    it("deve retornar erro se falhar ao enviar e-mail após a criação do funcionário", async () => {
+      req.body = {
+        nome: "Funcionario teste",
+        cpf: "19392392380",
+        endereco: "Rua A",
+        email: "funcionarioteste@exemplo.com",
+        funcao: "Dev",
+        salario: 5000,
+        dataNascimento: "2000-01-01",
+      };
+
+      // Mock das funções do modelo
+      Funcionarios.findOne
+        .mockResolvedValueOnce(null) // CPF não cadastrado
+        .mockResolvedValueOnce(null); // Email não cadastrado
+      Funcionarios.create.mockResolvedValue(req.body);
+
+      // Mock para falhar ao enviar o e-mail
+      emailNovoFuncionario.mockRejectedValue(new Error("Erro no envio"));
+
+      await funcionarioService.createFuncionario(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({
+        message: "Erro no envio",
+      });
+    });
   });
 
   describe("getFuncionarioById", () => {
@@ -156,6 +187,21 @@ describe("FuncionarioService", () => {
         message: "Funcionário encontrado com sucesso!",
       });
     });
+
+    it("deve retornar erro se ocorrer um erro interno ao buscar o funcionário", async () => {
+      req.params.id = 1;
+      // Simula um erro interno ao buscar o funcionário no BD
+      Funcionarios.findByPk.mockRejectedValueOnce(
+        new Error("Erro interno ao buscar o funcionário")
+      );
+
+      await funcionarioService.getFuncionarioById(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({
+        message: "Erro interno ao buscar o funcionário",
+      });
+    });
   });
 
   describe("getAllFuncionarios", () => {
@@ -170,6 +216,20 @@ describe("FuncionarioService", () => {
 
       expect(res.status).toHaveBeenCalledWith(200);
       expect(res.json).toHaveBeenCalledWith(funcionarios);
+    });
+
+    it("deve retornar erro se ocorrer um erro ao listar os funcionários", async () => {
+      // Simula um erro interno do BD ao listar os funcionarios
+      Funcionarios.findAll.mockRejectedValueOnce(
+        new Error("Erro interno ao listar")
+      );
+
+      await funcionarioService.getAllFuncionarios(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({
+        error: "Não foi possivel listar funcionários!",
+      });
     });
   });
 
@@ -199,6 +259,108 @@ describe("FuncionarioService", () => {
       expect(res.status).toHaveBeenCalledWith(200);
       expect(res.json).toHaveBeenCalledWith(servicos);
     });
+
+    it("deve retornar erro se ocorrer um erro ao buscar os serviços", async () => {
+      req.params.id = 1;
+
+      // Simula um erro interno ao buscar os serviços
+      Servicos.findAll.mockRejectedValueOnce(
+        new Error("Erro ao acessar os serviços")
+      );
+
+      await funcionarioService.getServicosByFuncionario(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({
+        message: "Erro ao acessar os serviços",
+      });
+    });
+  });
+
+  describe("updateFuncionario", () => {
+    it("deve retornar erro se o funcionário não for encontrado para atualizar", async () => {
+      req.params.id = 1;
+      Funcionarios.findByPk.mockResolvedValueOnce(null); // Funcionário inexistente
+
+      await funcionarioService.updateFuncionario(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(404);
+      expect(res.json).toHaveBeenCalledWith({
+        message: "Funcionário não encontrado.",
+      });
+    });
+
+    it("deve atualizar o funcionário se encontrado", async () => {
+      req.params.id = 1;
+      const funcionario = { id: 1, nome: "Teste", save: jest.fn() };
+      Funcionarios.findByPk.mockResolvedValueOnce(funcionario);
+
+      req.body = { nome: "Novo Nome" };
+
+      await funcionarioService.updateFuncionario(req, res);
+
+      expect(funcionario.save).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        funcionario,
+        message: "Dados atualizados do funcionario id=1 com sucesso!",
+      });
+    });
+
+    it("deve retornar erro se falhar ao salvar o funcionário", async () => {
+      req.params.id = 1;
+      const funcionario = { id: 1, nome: "Teste", save: jest.fn() };
+      Funcionarios.findByPk.mockResolvedValueOnce(funcionario);
+
+      req.body = { nome: "Novo Nome" };
+
+      // Mock para falha ao salvar
+      funcionario.save.mockRejectedValue(new Error("Erro ao salvar"));
+
+      await funcionarioService.updateFuncionario(req, res);
+
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({
+        message: "Erro ao salvar",
+      });
+    });
+    it("deve atualizar o nome do funcionário se o nome for fornecido", async () => {
+      req.params.id = 1;
+      const funcionario = { id: 1, nome: "Antigo Nome", save: jest.fn() };
+      Funcionarios.findByPk.mockResolvedValueOnce(funcionario);
+
+      req.body = { nome: "Novo Nome" };
+
+      await funcionarioService.updateFuncionario(req, res);
+
+      // Verifica se o nome foi atualizado
+      expect(funcionario.nome).toBe("Novo Nome");
+      expect(funcionario.save).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        funcionario,
+        message: "Dados atualizados do funcionario id=1 com sucesso!",
+      });
+    });
+
+    it("deve manter o nome do funcionário se o nome não for fornecido", async () => {
+      req.params.id = 1;
+      const funcionario = { id: 1, nome: "Antigo Nome", save: jest.fn() };
+      Funcionarios.findByPk.mockResolvedValueOnce(funcionario);
+
+      req.body = {}; // Nenhum nome fornecido
+
+      await funcionarioService.updateFuncionario(req, res);
+
+      // Verifica se o nome não foi alterado
+      expect(funcionario.nome).toBe("Antigo Nome");
+      expect(funcionario.save).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        funcionario,
+        message: "Dados atualizados do funcionario id=1 com sucesso!",
+      });
+    });
   });
 
   describe("deleteFuncionario", () => {
@@ -223,6 +385,22 @@ describe("FuncionarioService", () => {
 
       expect(res.status).toHaveBeenCalledWith(204);
       expect(funcionario.destroy).toHaveBeenCalled();
+    });
+  });
+
+  it("deve retornar erro se falhar ao enviar e-mail após a remoção", async () => {
+    req.params.id = 1;
+    const funcionario = { id: 1, nome: "Funcionario", destroy: jest.fn() };
+    Funcionarios.findByPk.mockResolvedValueOnce(funcionario);
+
+    // Mock para falhar ao enviar o e-mail
+    emailFuncionarioRemovido.mockRejectedValue(new Error("Erro no envio"));
+
+    await funcionarioService.deleteFuncionario(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "Erro no envio",
     });
   });
 });
